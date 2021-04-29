1edece9b1c71ff2526f0e5a2bd1cf905f60a41a1
Michael Rodriguez <mikeyrod22@gmail.com>
2021-04-29 12:33:48 -0500

    integrated front end

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const { animals } = require("./data/animals.json");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-const { animals } = require("./data/animals.json");
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
-  // Note that we save the animalsArray as filteredResults here:
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
-    // Save personalityTraits as a dedicated array.
-    // If personalityTraits is a string, place it into a new array and save.
     if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
-    // Loop through each trait in the personalityTraits array:
     personalityTraitsArray.forEach((trait) => {
-      // Check the trait against each animal in the filteredResults array.
-      // Remember, it is initially a copy of the animalsArray,
-      // but here we're updating it for each trait in the .forEach() loop.
-      // For each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one
-      // of the traits when the .forEach() loop is finished.
       filteredResults = filteredResults.filter(
         (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
@@ -50,7 +40,6 @@ function filterByQuery(query, animalsArray) {
       (animal) => animal.name === query.name
     );
   }
-  // return the filtered results:
   return filteredResults;
 }
 
@@ -107,6 +96,22 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
